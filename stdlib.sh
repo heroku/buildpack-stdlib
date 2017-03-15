22b616bbd76646de817139b71197eb949d9b2bbf
Kenneth Reitz <me@kennethreitz.org>
2017-03-15 14:39:35 -0400

    Update stdlib.sh

--- stdlib.sh
+++ stdlib.sh
@@ -58,3 +58,58 @@ sub-env() {
 
   )
 }
+
+# Logging
+# -------
+
+# Returns now, in milleseconds. Useful for logging. 
+# Example: let start=$(nowms); mtime "glide.install.time" "${start}"
+nowms() {
+    date +%s%3N
+}
+
+# Measures when an exit path to the buildpack is reached, given a name, then exits 1. 
+# Usage: $ countExit "binExists"
+countExit() {
+    count "error.${1}"
+    exit 1
+}
+
+# Log arbitrary data to the logfile (e.g. a packaging file). 
+# Usage: $ bplog "$(<${vendorJSON})
+bplog() {
+  echo -n ${@} | awk 'BEGIN {printf "msg=\""; f="%s"} {gsub(/"/, "\\\"", $0); printf f, $0} {if (NR == 1) f="\\n%s" } END { print "\"" }' >> ${BUILDPACK_LOG_FILE}
+}
+
+# Measures time elapsed for a specific build step. 
+# Usage: $ let start=$(nowms); mtime "glide.install.time" "${start}"
+mtime() {
+    local key="${BPLOG_PREFIX}.${1}"
+    local start="${2}"
+    local end="${3:-$(nowms)}"
+    echo "${key} ${start} ${end}" | awk '{ printf "measure#%s=%.3f\n", $1, ($3 - $2)/1000 }' >> ${BUILDPACK_LOG_FILE}
+}
+
+# Logs a count for a specific built step. 
+# Usage: $ count "tool.govendor"
+count() {
+    local k=”${BPLOG_PREFIX}.${1}”
+    local v=”${2:1}”
+    echo “count#${k}=${v}” >> ${BUILDPACK_LOG_FILE}
+}
+
+# Logs a measure for a specific built step. 
+# Usage: $ measure "tool.installed_dependencies" 42
+measure() {
+    local k=”${BPLOG_PREFIX}.${1}”
+    local v=”${2}”
+    echo “measure#${k}=${v}” >> ${BUILDPACK_LOG_FILE}
+}
+
+# Logs a unuique measurement build step. 
+# Usage: $ measure "build_id" $BUILD_ID
+unique() {
+    local k="${BPLOG_PREFIX}.${1}"
+    local v=”${2}”
+    echo "unique#${k}=${v}" >> ${BUILDPACK_LOG_FILE}
+}
