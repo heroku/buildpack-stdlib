bb41b1fa52cc751a881d6142f7c794e1744f7410
Kenneth Reitz <me@kennethreitz.org>
2017-05-04 17:35:19 -0400

    trying travis again

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/awk BEGIN{a=ARGV[1];sub(/[a-z_.]+$/,"/bats/bin/bats",a);system(a"\t"ARGV[1])}
+#!/usr/bin/awk BEGIN{a=ARGV[1];sub(/[a-z_.]+$/,"bats/bin/bats",a);system(a"\t"ARGV[1])}
 # ^^ Crazy hack to get relative paths to work w/ shebangs.
 
-source ../stdlib.sh
+source stdlib.sh
 
 
 setup() {
@@ -54,6 +54,18 @@ teardown() {
   [ "$result1" = "export hello=world" ]
   [ "$result2" = "export hello=world" ]
 }
+
+@test "results of set-env" {
+
+  set-env hello world
+
+  result1="$(cat $PROFILE_PATH)"
+  result2="$(cat $EXPORT_PATH)"
+
+  [ "$result1" = "export hello=world" ]
+  [ "$result2" = "export hello=world" ]
+}
+
 
 @test "results of set-default-env" {
 
